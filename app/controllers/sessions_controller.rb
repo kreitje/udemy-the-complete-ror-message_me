554cd80802316db3c877863d5a385b1f10dd6932
Jeff Kreitner <jeff@hitmyserver.com>
2019-03-27 22:46:32 -0400

    Login and flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,24 @@ class SessionsController < ApplicationController
   def new
 
   end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id;
+      flash[:success] = "Welcome back #{user.username}"
+      redirect_to root_path
+    else
+      flash.now[:error] = "Invalid username or password"
+      render 'new'
+    end
+
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have been logged out"
+    redirect_to login_path
+  end
+
 end
